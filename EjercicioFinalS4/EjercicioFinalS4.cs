5fd66bd5533d76cb047e018b8e1339c72412c434
BXD <Benixered@gmail.com>
2023-05-29 10:34:08 -0600

    terminado

--- EjercicioFinalS4/EjercicioFinalS4.cs
+++ EjercicioFinalS4/EjercicioFinalS4.cs
@@ -14,18 +14,50 @@ class Program
     Libro l1 = new Libro("Dune", "Frank Herbert");
     Libro l2 = new Libro("7 habitos de la gente altamente efectiva", "John Maxwell");
 
-//-----------------------------------------------------------------------------
+    //-----------------------------------------------------------------------------
     Biblioteca biblioteca = new Biblioteca();
     biblioteca.agregar(r1);
     biblioteca.agregar(l1);
     biblioteca.agregar(l2);
 
-//-----------------------------------------------------------------------------
-    biblioteca.prestar(r1);
-    biblioteca.devolver(r1); 
+    //-----------------------------------------------------------------------------
+    Console.WriteLine("Quiere prestar una revista?");
+    string resp = Console.ReadLine();
+    if (resp == "si")
+    {
+      biblioteca.prestar(r1);
+    }
+    else if (resp == "no")
+    {
+      Console.WriteLine("Quieres devolver una revista?");
+      if (resp == "si")
+      {
+        biblioteca.devolver(r1);
+      }
+      else if (resp == "no")
+      {
+        break;
+      }
+    }
 
-    biblioteca.prestar(l1); 
-    biblioteca.devolver(l1); 
-
+    Console.WriteLine("Quiere prestar un libro?");
+    string resp2 = Console.ReadLine();
+    if (resp2 == "si")
+    {
+      biblioteca.prestar(l1);
+    }
+    else if (resp2 == "no")
+    {
+      Console.WriteLine("Quieres devolver un libro?");
+      if (resp2 == "si")
+      {
+        biblioteca.devolver(l1);
+      }
+      else if (resp2 == "no")
+      {
+        break;
+      }
+    }
+    Console.readKey();
   }
 }
